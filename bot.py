638c1d9e9bf944e804040f6229dbb9680db2079c
Feruz <maxmudovferuz78@gmail.com>
2026-09-17 22:03:22 +0500

    yangilandi

---
+++ bot.py
@@ -0,0 +1,19 @@
+import asyncio
+import logging
+import uuid
+
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.types import (
+    Message,
+    CallbackQuery,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+    FSInputFile,
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+)
+
