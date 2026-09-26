c4a68d165eefe703d24c09e82eed98e663ec7713
Feruz <maxmudovferuz78@gmail.com>
2026-09-26 22:27:52 +0500

    yangilandi

---
+++ test_openai.py
@@ -0,0 +1,9 @@
+"""
+OpenAI ulanishini alohida sinash uchun.
+Ishga tushirish: python test_openai.py
+"""
+import asyncio
+from openai import AsyncOpenAI
+from config import OPENAI_API_KEY, OPENAI_MODEL
+
+
